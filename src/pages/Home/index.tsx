020db519b7f932d89f32d0e668e7fdeea798a58c
unluna <unluna@unlunadeMacBook-Pro.local>
2025-06-07 23:40:00 +0800

    fix(index.tsx): deepseek + gl 链路打通

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,35 +1,60 @@
-import Guide from '@/components/Guide';
-import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import { request } from '@umijs/max';
-import { Input } from 'antd';
-import { useEffect, useState } from 'react';
+import { Button, Input } from 'antd';
+import { useCallback, useState } from 'react';
 import styles from './index.less';
 
 const HomePage: React.FC = () => {
-  // const { name } = useModel('global');
+  // const [username, setUsername] = useState<string>();
   const [questionText, setQuestionText] = useState<string>();
-  useEffect(() => {
-    // request('/api', {
-    request('https://website-worker.729769298.workers.dev/', {
-      method: 'GET',
+  const [answerList, setAnswerList] = useState<string[]>([]);
+
+  const ajaxAskDeepSeek = useCallback(() => {
+    request('https://website-worker.729769298.workers.dev/graphql', {
+      // request('http://localhost:8787/', {
+      method: 'POST',
+      data: {
+        query: `
+        query($prompt: String!) {
+          ask(prompt: $prompt) {
+            errno
+            result
+            error
+          }
+        }
+      `,
+        variables: {
+          prompt: questionText,
+        },
+      },
     }).then((res) => {
-      console.log('res', res);
+      setAnswerList([...answerList, res?.data?.ask?.result]);
     });
-  }, []);
+  }, [questionText, answerList, setAnswerList]);
 
   return (
     <PageContainer ghost>
       <div className={styles.container}>
-        <div>
-          请输入问题：
-          <Input
-            value={questionText}
-            onChange={(e) => setQuestionText(e.target.value)}
-          />
+        <div style={{ maxWidth: '800px' }}>
+          {/*<div>*/}
+          {/*  请输入用户：*/}
+          {/*  <Input*/}
+          {/*    value={username}*/}
+          {/*    onChange={(e) => setUsername(e.target.value)}*/}
+          {/*  />*/}
+          {/*</div>*/}
+          <div>
+            请输入问题：
+            <Input
+              value={questionText}
+              onChange={(e) => setQuestionText(e.target.value)}
+            />
+          </div>
+          <Button onClick={() => ajaxAskDeepSeek()}>发送</Button>
         </div>
-
-        <Guide name={trim('白世玉的网站')} />
+        {answerList.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
       </div>
     </PageContainer>
   );
